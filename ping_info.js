53c4571fc1859f27476020c3701cb8e509d6e628
Kaito-Von-Vocaloid <84059078+Kaito-Von-Vocaloid@users.noreply.github.com>
2021-11-19 13:00:56 +0300

    Create ping_info.js

---
+++ ping_info.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+const embedColor = '#56F5B3';
+const prefix = '!';
+
+function ping(msg){
+	return(`Gecikme: ${Date.now() - msg.createdTimestamp}\nDiscord geçikmesi: ${Math.round(client.ws.ping)}`);
+}
+
+client.on('messageCreate', message => {
+	if(message.mentions.users.first() == client.user){
+		const iEmbed = new Discord.MessageEmbed()
+		.setColor(embedColor)
+		.setTitle('Bot hakkında bazı bilgiler')
+		.setDescription(`Bot prefix: ${prefix}\nPing: \n${ping(message)}`)
+		.setFooter('Etiket-Bilgi sistemi')
+		.setTimestamp();
+		message.channel.send({ embeds: [iEmbed] });
+	}
+});
+
+client.on('ready', () => {
+	console.log('!');
+});
+
+client.login(process.env['TOKEN']);
