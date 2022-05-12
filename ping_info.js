853475f568d32f9700739bfc192881033a598159
Kaito-Von-Vocaloid <84059078+Kaito-Von-Vocaloid@users.noreply.github.com>
2022-05-12 19:44:25 +0300

    Update ping_info.js

--- ping_info.js
+++ ping_info.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 const embedColor = '#56F5B3';
 
 function ping(msg){
